d90cee9cf20ac1c5a3e6aad7d9bf07d3c234e496
Артем <qiwihere@gmail.com>
2018-12-23 16:49:14 +0300

    Test

--- TelegramBot.py
+++ TelegramBot.py
@@ -5,7 +5,7 @@ dispatcher = updater.dispatcher
 
 
 
-# Обработка команд для бота
+# Обработка команд для бота телеги
 def startCommand(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
 
