824036746dbbc2d71c855eea503a3c8f4111c091
Артем <qiwihere@gmail.com>
2018-12-24 00:51:37 +0300

    update

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,21 +1,15 @@
+# Настройки
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import Bot
-updater = Updater(token='623317837:AAHwNgxSD9Kbz2Tz2NBKewVhNUGYZXNJ6jg')
+updater = Updater(token='ВАШ API ТОКЕН') # Токен API к Telegram
 dispatcher = updater.dispatcher
-
-
-
-# Обработка команд для бота телеги лол
+# Обработка команд
 def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся или нет?')
-
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
 def textMessage(bot, update):
     response = 'Получил Ваше сообщение: ' + update.message.text
     bot.send_message(chat_id=update.message.chat_id, text=response)
-
 # Хендлеры
 start_command_handler = CommandHandler('start', startCommand)
-
 text_message_handler = MessageHandler(Filters.text, textMessage)
 # Добавляем хендлеры в диспетчер
 dispatcher.add_handler(start_command_handler)
