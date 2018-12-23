75367321c63dae0f8d7af577dcb651af0fd0cff8
Артем <qiwihere@gmail.com>
2018-12-23 17:01:53 +0300

    234234

--- TelegramBot.py
+++ TelegramBot.py
@@ -7,7 +7,7 @@ dispatcher = updater.dispatcher
 
 # Обработка команд для бота телеги лол
 def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся или нет?')
 
 def textMessage(bot, update):
     response = 'Получил Ваше сообщение: ' + update.message.text
