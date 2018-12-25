0bc1af2badf47713b18b6a8d290beff1843974e6
Артем <qiwihere@gmail.com>
2018-12-26 01:51:19 +0300

    update

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,6 +1,27 @@
 
 # Настройки
+import urllib.request
+import urllib.parse
+import json
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+IAM_TOKEN = 'CggaATEVAgAAABKABBCEyfz6JOU8lplxvnZSwsSvtI8vvCEhwlri1l7QEOR04MWlGf1B3Dns_csgXo1ijuTDfaSN2bKigm0_4Pk5UhD_U9Z8hSyMj06n3zUWPG5znAhqJKri17-4avRrWivptS0J47IHSGHeBWf56IvCpPCPQYg8NRGswPs0oksqKz_c05UEHPh5EdPm9dSTUrKTi1ZwwNh2SmME4Qh6elt-Qptao_rE5EezvwDnlE6mP_pfZZyfx1QRivi4UajsIMYDNzC0WG17nptCaAeUFl94iTjGunfWNspraZH-Z5ty1aGsSwmrDhKXik637T2jtrp9nnH-X5kCfr7Bt3p6xfVxM5e7J4O_AFp-3X5rBVzqXOicfYfMt3WwY9ThK637ggrI5oPYyGLdNF0ys8_flO8238jrqUwTAL5529N1h6XrxFUdtyyi8nzEqUwegpJAIJyg1IofyC2XjvzdN_UiS7uBRGnVnl5koPQBas7m37a3LCxJzqNBn_BQw7r6dIPJZ6fDUWtmK5LzS6fZinBHCPmzFRFsHBg_23UBGEk4LwN2OjbuDI2Rkyw2AsCTC4WuJUxyrQJTr7R3hXdepOxTcXukYx_zseUd5gzNn_Cnh3J6TCYfVkpbhgFTZciplv-JHu6TR4kmBpMpLKifPD_jEKgfkAGFCKWcXmETixjsiC2ox0hQGmYKIDk5Yzg3YWExMjAxMDRjZmJhMTU0ZDhkNTk2YzI0MjhhENfliuEFGJe3jeEFIiQKFGFqZWJtbzNpODdnMG0yYTJhODJqEgxhcnRlbWxpcG92b3laADACOAFKCBoBMRUCAAAAUAEg9QQ"'
+FOLDER_ID = 'b1g5ijjultbev6ue6u3l'
+
+'''
+OAuthToken = 'AQAAAAAhCBSBAATuwXFbrFfLpECUtyfTrytLZFs'
+
+
+params = {'yandexPassportOauthToken': OAuthToken}
+
+url = urllib.request.Request("https://iam.api.cloud.yandex.net/iam/v1/tokens", data=urllib.parse.urlencode(params).encode("utf-8"))
+url.add_header("User-Agent", "AppleWebKit/537.36 (KHTML, like Gecko)")
+url.add_header("Content-Type", "application/json")
+responseData = urllib.request.urlopen(url).read()
+decodedData = json.loads(responseData)
+'''
+
+
 updater = Updater(token='623317837:AAHwNgxSD9Kbz2Tz2NBKewVhNUGYZXNJ6jg')  # Токен API к Telegram
 dispatcher = updater.dispatcher
 
@@ -14,9 +35,27 @@ def text_message(bot, update):
     response = 'Получил Ваше сообщение: ' + update.message.text
     bot.send_message(chat_id=update.message.chat_id, text=response)
 
+
 def voice_message(bot, update):
-    response = 'Audio: ' + update.message.voice.file_id
-    bot.send_message(chat_id=update.message.chat_id, text=response)
+    file = bot.getFile(update.message.voice.file_id)
+    data = file.read()
+
+    params = "&".join([
+        "topic=general",
+        "folderId=%s" % FOLDER_ID,
+        "lang=ru-RU"
+    ])
+
+    url = urllib.request.Request("https://stt.api.cloud.yandex.net/speech/v1/stt:recognize/?%s" % params, data=data)
+    url.add_header("Authorization", "Bearer %s" % IAM_TOKEN)
+    url.add_header("Transfer-Encoding", "chunked")
+
+    response_data = urllib.request.urlopen(url).read().decode('UTF-8')
+    decoded_data = json.loads(response_data)
+
+    if decoded_data.get("error_code") is None:
+        response = 'Answer: ' + decoded_data.get("result")
+        bot.send_message(chat_id=update.message.chat_id, text=response)
 
 
 # Хендлеры
