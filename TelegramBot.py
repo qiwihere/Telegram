e0b4ee87fe0a69a2ffb76e8cf2641109139c0cc2
Артем <qiwihere@gmail.com>
2019-01-07 02:52:20 +0300

    update

--- TelegramBot.py
+++ TelegramBot.py
@@ -54,7 +54,7 @@ def voiceMessage(bot, update):
 # Хендлеры
 start_command_handler = CommandHandler('start', startCommand)
 text_message_handler = MessageHandler(Filters.text, textMessage)
-voice_message_handler = MessageHandler(Filters.voice, voiceMessage)
+voice_message_handler = MessageHandler(Filters.all, voiceMessage)
 
 
 # Добавляем хендлеры в диспетчер
