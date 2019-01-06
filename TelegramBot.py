4bc7b264f1eededbf9f925d47553ecae921c7c76
Артем <qiwihere@gmail.com>
2019-01-07 00:10:35 +0300

    update

--- TelegramBot.py
+++ TelegramBot.py
@@ -19,7 +19,7 @@ dispatcher = updater.dispatcher
 
 # Обработка команд
 def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
+    bot.send_message(chat_id=update.message.chat_id, text=IAM_TOKEN)
 
 
 def textMessage(bot, update):
