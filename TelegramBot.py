6fc7d01302bf3cfb0587e61f8656090b91d032e8
Артем <qiwihere@gmail.com>
2019-01-07 02:05:32 +0300

    update

--- TelegramBot.py
+++ TelegramBot.py
@@ -34,7 +34,7 @@ def voiceMessage(bot, update):
 
     data = open(path, 'rb').read()
     headers = {
-        'Authorization': 'Bearer '+IAM_TOKEN,
+    #    'Authorization': 'Bearer '+IAM_TOKEN,
         'Transfer-Encoding': 'chunked',
     }
 
