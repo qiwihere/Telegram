acd53452e7d2577ce1e3b1cc7c025d947e7b39ff
Артем <qiwihere@gmail.com>
2019-01-09 01:32:13 +0300

    update

--- TelegramBot.py
+++ TelegramBot.py
@@ -61,10 +61,10 @@ def voiceMessage(bot, update):
         url = urllib.request.Request("https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize/?%s" % params)
         url.add_header("Authorization", "Bearer %s" % IAM_TOKEN)
         url.add_header("Transfer-Encoding", "chunked")
-        responseData = urllib.request.urlopen(url).read()
+        #responseData = urllib.request.urlopen(url).read()
 
         bot.send_message(chat_id=update.message.chat_id, text=translated)
-        bot.send_voice(chat_id=update.message.chat_id, voice=open(responseData,'wb'))
+        #bot.send_voice(chat_id=update.message.chat_id, voice=open(responseData,'wb'))
 
 
 voice_message_handler = MessageHandler(Filters.voice, voiceMessage)
