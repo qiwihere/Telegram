5e61027f6113be7a0034d2cdba97d43ff727993a
Артем <qiwihere@gmail.com>
2019-01-09 02:35:18 +0300

    update

--- TelegramBot.py
+++ TelegramBot.py
@@ -63,12 +63,12 @@ def voiceMessage(bot, update):
         url = urllib.request.Request("https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize/?%s" % params)
         url.add_header("Authorization", "Bearer %s" % IAM_TOKEN)
         url.add_header("Transfer-Encoding", "chunked")
-        responseData = urllib.request.urlopen(url).read()
-        f = open("ttsaudio.ogg", "wb")
-        f.write(responseData)
-        f.close()
+        #responseData = urllib.request.urlopen(url).read()
+       # f = open("ttsaudio.ogg", "wb")
+       # f.write(responseData)
+       # f.close()
         bot.send_message(chat_id=update.message.chat_id, text=translated)
-        bot.send_voice(chat_id=update.message.chat_id, voice=open('ttsaudio.ogg', 'rb'))
+      #  bot.send_voice(chat_id=update.message.chat_id, voice=open('ttsaudio.ogg', 'rb'))
 
 
 voice_message_handler = MessageHandler(Filters.voice, voiceMessage)
